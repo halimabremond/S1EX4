3096cccea1146df12f7d60232bc9e87c3b5dee5e
halimabremond <halima.bremond@yahoo.com>
2021-02-18 14:28:58 +0100

    x4

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gendre = "Monsieur";
+            string name = "Vincent";
+
+            Console.WriteLine("Bonjour " + gendre + " "+name+ ",vous êtes venu nous rendre visite 1 fois");
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
